1e7a90fed23f72c98987e7ad17c0b7c958b3d05d
jmeno1011 <jwhltn8282@gmail.com>
2023-08-16 18:14:24 +0900

    edit hightlight code step

--- src/components/UI/ChartJS/Test.tsx
+++ src/components/UI/ChartJS/Test.tsx
@@ -1,21 +1,15 @@
-import React from 'react'
+import React from "react";
 import Highlight from "react-highlight";
 
 export default function Test() {
   return (
     <div>
-        <h1>Test</h1>
-        <Highlight className="javascript">{
-            `import React from 'react'
-            export default function Test() {
-              return (
-                <div>Test</div>
-              )
-            }`
-        }</Highlight>
+      <h1>Test</h1>
+      <Highlight className="react">{`export default function Test() {
+    return (
+        <div>Test</div>
+    )
+}`}</Highlight>
     </div>
-  )
+  );
 }
-
-
-
