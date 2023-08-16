4816bfcc562c0ede7d7492c38692ef85aa547590
jmeno1011 <jwhltn8282@gmail.com>
2023-08-16 11:23:25 +0900

    add pages and edit baseUrl in tsconfig.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import './App.css';
+import {Route, Routes } from 'react-router-dom';
+import { About, Home } from 'pages';
 
 function App() {
   return (
-    <div></div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
   );
 }
 
