5dcf28308fa6e6089319e7d91c91e60178cb8293
jmeno1011 <jwhltn8282@gmail.com>
2023-09-08 10:42:33 +0900

    edit nav, add p-dashboard page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import { About, Home } from "pages";
 import Layout from "layout/Layout";
+import PDashboard from "pages/dashboard1/PDashboard";
 
 function App() {
   return (
@@ -10,6 +11,9 @@ function App() {
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
         <Route path="/about" element={<About />} />
+      </Route>
+      <Route path="/p-dashboard" element={<PDashboard />}>
+
       </Route>
     </Routes>
   );
--- src/components/common/Nav/Nav.tsx
+++ src/components/common/Nav/Nav.tsx
@@ -13,6 +13,11 @@ export default function Nav() {
                     <li className={styles.li}><Link to={"/"}>Home</Link></li>
                     <li className={styles.li}><Link to={"/about"}>About</Link></li>
                 </ul>
+                <hr className={styles.hr} />
+                <h5 className={styles.h5}>PJ</h5>
+                <ul className={styles.ulI}>
+                    <li className={styles.li}><Link to={"/p-dashboard"}>P-Dashboard</Link></li>
+                </ul>
             </div>
         </div>
     </nav>
