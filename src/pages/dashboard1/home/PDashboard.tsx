dce53e673b28b4e7ad5264ff4c311840e92c85de
jmeno1011 <jwhltn8282@gmail.com>
2023-09-08 17:47:40 +0900

    remove comment code

--- src/pages/dashboard1/home/PDashboard.tsx
+++ src/pages/dashboard1/home/PDashboard.tsx
@@ -1,16 +1,9 @@
 import React from 'react'
 import styles from "./PDashboard.module.css"
-import Nav from 'components/PDashboard/common/Nav/Nav'
-import Header from 'components/PDashboard/common/Header/Header'
 
 export default function PDashboard() {
   return (
     <div className={styles.container}>
-      {/* <Header />
-      <div className={styles.wrapper}>
-        <Nav />
-        <main></main>
-      </div> */}
       PDashboard
     </div>
   )
