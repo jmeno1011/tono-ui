65c5713c45260b91026566bcd1e3324a67cb7074
jmeno1011 <jwhltn8282@gmail.com>
2023-09-08 16:01:56 +0900

    add login page

--- src/pages/dashboard1/login/PLogin.tsx
+++ src/pages/dashboard1/login/PLogin.tsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { ChangeEvent, FormEvent, useState } from 'react'
+import { useDispatch } from "react-redux";
 import styles from "./PLogin.module.css"
+import { login } from 'redux/features/auth';
 
 export default function PLogin() {
+  const dispatch = useDispatch();
+  const [id, setId] = useState('test');
+  
+  const onChange = (e:ChangeEvent<HTMLInputElement>)=>{
+    setId(e.target.value);
+  }
+  const onSubmit = (e:FormEvent<HTMLFormElement>)=>{
+    e.preventDefault();
+    const logData = {
+      div: 'p',
+      user: id
+    }
+    dispatch(login(logData));
+  }
   return (
-    <div>PLogin</div>
+    <div className={styles.container}>
+      <h3>Sign In</h3>
+      <form className={styles.form} onSubmit={onSubmit}>
+        <input type="text" id="id" value={id} onChange={onChange} />
+        <button>Sign in</button>
+      </form>
+    </div>
   )
 }
