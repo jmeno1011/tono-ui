5d31cd0e766cac65481ce00aa885ddaff08946b9
jmeno1011 <jwhltn8282@gmail.com>
2023-10-05 18:01:48 +0900

    add state charts

--- src/components/PDashboard/State/HorizontalBarChart/HorizontalBarChart.tsx
+++ src/components/PDashboard/State/HorizontalBarChart/HorizontalBarChart.tsx
@@ -1,6 +1,58 @@
 import React from "react";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
 import styles from "./HorizontalBarChart.module.css";
+import { ChartTitle } from "components/PDashboard/common";
+import { RootState } from "store";
+import { useSelector } from "react-redux";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 export default function HorizontalBarChart() {
-  return <div className={styles.container}>HorizontalBarChart</div>;
+  const { surveyResult } = useSelector((state: RootState) => state.pDash);
+  return (
+    <div className={styles.container}>
+      <ChartTitle>HorizontalBarChart</ChartTitle>
+      <Bar options={options} data={{
+              labels: surveyResult.map((survey) => survey.SEQ),
+              datasets: [
+                {
+                  label: "total",
+                  data: surveyResult.map((survey) => survey.TOTAL),
+                  backgroundColor: "rgb(103, 80, 164)",
+                },
+              ],
+            }} />
+    </div>
+  );
 }
+
+const options = {
+  indexAxis: "y" as const,
+  elements: {
+    bar: {
+      borderWidth: 2,
+    },
+  },
+  responsive: true,
+  plugins: {
+    legend: {
+      display: false,
+    }
+  },
+};
--- src/components/PDashboard/State/StackChart/StackChart.tsx
+++ src/components/PDashboard/State/StackChart/StackChart.tsx
@@ -1,8 +1,73 @@
 import React from 'react'
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+
 import styles from "./StackChart.module.css"
+import { ChartTitle } from 'components/PDashboard/common';
+import { RootState } from 'store';
+import { useSelector } from 'react-redux';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 export default function StackChart() {
+  const { surveyResult } = useSelector((state: RootState) => state.pDash);
+
   return (
-    <div className={styles.container}>StackChart</div>
+    <div className={styles.container}>
+      <ChartTitle>Figure Stack</ChartTitle>
+      <Bar options={options} data={
+        {
+          labels: surveyResult.filter(survey=> survey.PROCESS_STATE !== "진행중").map((survey) => survey.SEQ),
+          datasets: [
+            {
+              label: "FIGURE1",
+              data: surveyResult.filter(survey=> survey.PROCESS_STATE !== "진행중").map((survey) => survey.FIGURE1),
+              borderColor: "rgb(103, 80, 164)",
+              backgroundColor: "rgb(103, 80, 164, 0.5)",
+            },
+            {
+              label: "FIGURE2",
+              data: surveyResult.filter(survey=> survey.PROCESS_STATE !== "진행중").map((survey) => survey.FIGURE2),
+              borderColor: "rgb(157, 179, 88)",
+              backgroundColor: "rgb(157, 179, 88, 0.5)",
+            },
+            {
+              label: "FIGURE3",
+              data: surveyResult.filter(survey=> survey.PROCESS_STATE !== "진행중").map((survey) => survey.FIGURE3),
+              borderColor: "rgb(48, 74, 150)",
+              backgroundColor: "rgb(48, 74, 150, 0.5)",
+            },
+          ],
+        }
+      } />
+    </div>
   )
 }
+
+const options = {
+  indexAxis: 'y' as const,
+  responsive: true,
+  scales: {
+    x: {
+      stacked: true,
+    },
+    y: {
+      stacked: true,
+    },
+  },
+};
