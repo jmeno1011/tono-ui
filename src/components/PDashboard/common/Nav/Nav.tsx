0b7c726a7eaa13fedcd4931e23cc532d10aad5e5
jmeno1011 <jwhltn8282@gmail.com>
2023-11-11 16:11:56 +0900

    edit nav

--- src/components/PDashboard/common/Nav/Nav.tsx
+++ src/components/PDashboard/common/Nav/Nav.tsx
@@ -5,12 +5,12 @@ import styles from "./Nav.module.css";
 const cn = classNames.bind(styles);
 
 export default function Nav() {
-  const {pathname} = useLocation();
+  const { pathname } = useLocation();
   return (
     <nav className={styles.container}>
       <ul>
-        <li className={cn('li', pathname==="/p-dashboard" && 'active')}><Link to={"/p-dashboard"}>dashboard</Link></li>
-        <li className={cn('li', pathname==="/p-dashboard/p-survey" && 'active')}><Link to={"/p-dashboard/p-survey"}>survey</Link></li>
+        <li className={cn('li', pathname.includes("/p-dashboard") && 'active')}><Link to={"/p-dashboard"}>dashboard</Link></li>
+        <li className={cn('li', pathname === "/p-dashboard/p-survey" && 'active')}><Link to={"/p-dashboard/p-survey"}>survey</Link></li>
       </ul>
     </nav>
   )
